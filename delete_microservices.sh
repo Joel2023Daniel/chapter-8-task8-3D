851b3e91d0f7fe42774b1de31f207ba14fe1a94c
Joel2023Daniel <s219504809@deakin.edu.au>
2024-08-30 16:57:14 +1000

    Trigger change in gateway

---
+++ delete_microservices.sh
@@ -0,0 +1,7 @@
+kubectl delete -f scripts/cd/gateway.yaml
+kubectl delete -f scripts/cd/history.yaml
+kubectl delete -f scripts/cd/metadata.yaml
+kubectl delete -f scripts/cd/mongodb.yaml
+kubectl delete -f scripts/cd/rabbit.yaml
+kubectl delete -f scripts/cd/video-streaming.yaml
+kubectl delete -f scripts/cd/video-upload.yaml
